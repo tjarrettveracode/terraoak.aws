2ebe633fba1b74fea7bdd36bba811be26c265a54
morgan-oak9 <msuch@oak9.io>
2023-03-14 15:27:22 -0500

    moving contextual configs into separate repo

--- terraform.aws/s3/s3-bucket.tf
+++ terraform.aws/s3/s3-bucket.tf
@@ -1,3 +1,8 @@
+resource "aws_s3_bucket" "s3_bucket_sac" {
+  force_destroy       = false
+  object_lock_enabled = false
+}
+
 resource "aws_s3_bucket_ownership_controls" "s3_ownership_controls_sac" {
   bucket = aws_s3_bucket.s3_bucket_sac.id
 
